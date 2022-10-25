438c72b6360935bd15c5fb39866ec5fa50e12305
Eduardo Salcedo <esalcedo@mdc.edu>
2022-10-25 16:52:39 -0400

    Add view

--- MyResume/ViewController.swift
+++ MyResume/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    
+
+    
+    // MARK: - Instance Properties
+    let phoneNumber = "7862983357"
+    
+    //MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - IB Actions
+    @IBAction func callMeButtonTapped(_ sender: UIButton) {
+        if let url = NSURL(string: "tel://\(phoneNumber)") {
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+            // next line of code demonstrates that the call button will work on a physical device. This is inserted to demo functionality.
+            // since the simulator cannot make a phone call, the next line will print a string to the console.
+            print("A physical device would be calling my number. Voicemail will be automatically invoked (since I am calling my number from my own device).")
+        }
+        // NSURL stands for Next Step URL
+        // "tel:// = URL scheme - used to access the telephone and access this value
+    }
+    
+    
+    
+    //MARK: - Instance Methods
 
 }
 
---
+++ README.md
@@ -0,0 +1,2 @@
+#  MyResume 📄
+A three screen application that will enable the user to call my number, display my blog, and display a PDF of my resume that can be shared using the action sheet.
